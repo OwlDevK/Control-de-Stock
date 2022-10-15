2f5bdb52485ba2d42d1bf494337dc7d4342ada4c
Kenneth Porras <owlphilologist@gmail.com>
2022-10-14 19:59:03 -0600

    CRUD finalizado

---
+++ src/main/java/com/owldev/jdbc/ControlDeStockMain.java
@@ -0,0 +1,14 @@
+package com.owldev.jdbc;
+
+import javax.swing.JFrame;
+
+import com.owldev.jdbc.view.ControlDeStockFrame;
+
+public class ControlDeStockMain {
+
+	public static void main(String[] args) {
+		ControlDeStockFrame produtoCategoriaFrame = new ControlDeStockFrame();
+		produtoCategoriaFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
